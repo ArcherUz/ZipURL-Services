212f02a7c4ab792705bdecf34be2d6a8f9224bcc
ArcherUz <Yan199972>
2024-02-22 23:52:31 -0700

    change to gradle

--- urlsconvert/src/main/java/com/example/urlsconvert/utils/JwtUtil.java
+++ urlsconvert/src/main/java/com/example/urlsconvert/utils/JwtUtil.java
@@ -3,27 +3,37 @@ package com.example.urlsconvert.utils;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
 @Component
 public class JwtUtil {
     @Value("${jwt.secret}")
-    private String secretKey;
+    private String secretKeyString;
+
+    private Key secretKey;
 
     @Value("${jwt.expiration}")
     private long tokenValidity;
 
-    public String generateToken(String username){
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
+    }
+
+    public String generateToken(String username) {
         return Jwts.builder()
                 .setIssuer("ZipURL")
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + tokenValidity * 1000))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -50,6 +60,10 @@ public class JwtUtil {
     }
 
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
