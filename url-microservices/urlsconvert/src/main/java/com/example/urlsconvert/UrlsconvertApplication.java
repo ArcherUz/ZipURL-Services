3807d2c05f4144315aecde3eec025acad0f5e33a
ArcherUz <Yan199972>
2024-04-05 16:23:24 -0600

    eureka server connected

--- url-microservices/urlsconvert/src/main/java/com/example/urlsconvert/UrlsconvertApplication.java
+++ url-microservices/urlsconvert/src/main/java/com/example/urlsconvert/UrlsconvertApplication.java
@@ -3,11 +3,12 @@ package com.example.urlsconvert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@EnableCaching
-
+@EnableDiscoveryClient
 public class UrlsconvertApplication {
 
     public static void main(String[] args) {
